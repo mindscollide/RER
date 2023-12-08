0041022f22281694065cd25130f4af0184f794bc
Huzeifa Jahangir <84620132+huzeifajaha@users.noreply.github.com>
2023-12-08 18:46:17 +0500

    changes

--- src/commen/functions/utils.js.js
+++ src/commen/functions/utils.js.js
@@ -1,9 +1,86 @@
 export function getItem(label, key, icon, children, type) {
-    return {
-      key,
-      icon,
-      children,
-      label,
-      type,
-    };
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+// Your generateMenuItems function
+export function generateMenuItems(roleID, t) {
+  const BranchAdmin = [
+    getItem(
+      <div className="setup-text-style">{t("Setups")}</div>,
+      "sub1",
+      <i className="icon-settings side-bar-icons"></i>,
+      [
+        getItem(t("Shift"), "2"),
+        getItem(t("Counter-admin"), "3"),
+        getItem(t("Branch-service"), "4"),
+      ],
+      "menu-item-sub"
+    ),
+    getItem(
+      <div className="setup-text-style">{t("Reports")}</div>,
+      "sub2",
+      <i className="icon-file side-bar-icons"></i>,
+      [
+        getItem(t("Country-wise-reports"), "12"),
+        getItem(t("City-wise-reports"), "13"),
+        getItem(t("Branch-wise-reports"), "14"),
+        getItem(t("Employee-wise-reports"), "15"),
+        getItem(t("Service-wise-reports"), "16"),
+      ],
+      "menu-item-sub"
+    ),
+  ];
+  const CityAdmin = [
+    getItem(
+      <div className="setup-text-style">{t("Setups")}</div>,
+      "sub1",
+      <i className="icon-settings side-bar-icons"></i>,
+      [
+        getItem(t("City-admin"), "5"),
+        getItem(t("City-wise-branch-services"), "6"),
+        getItem(t("City-branch-wise-shifts"), "7"),
+        getItem(t("City-branch-shifts-wise-counter"), "8"),
+        getItem(t("Employee-main"), "9"),
+        getItem(t("Country-city-wise-counter"), "10"),
+        getItem(t("Country-main"), "11"),
+      ],
+      "menu-item-sub"
+    ),
+    getItem(
+      <div className="setup-text-style">{t("Reports")}</div>,
+      "sub2",
+      <i className="icon-file side-bar-icons"></i>,
+      [
+        getItem(t("Country-wise-reports"), "12"),
+        getItem(t("City-wise-reports"), "13"),
+        getItem(t("Branch-wise-reports"), "14"),
+        getItem(t("Employee-wise-reports"), "15"),
+        getItem(t("Service-wise-reports"), "16"),
+      ],
+      "menu-item-sub"
+    ),
+  ];
+  // Check roleID and append specific items
+  if (roleID === "4") {
+    // Add items specific to roleID 4
+    return BranchAdmin;
+  } else if (roleID === "3") {
+    // Add items specific to roleID 5
+    return CityAdmin;
   }
+
+  // Default items if roleID doesn't match any condition
+  return [
+    getItem(
+      <div className="setup-text-style">{t("Default-menu-item")}</div>,
+      "defaultKey"
+    ),
+    // Add other default items if needed
+    // ...
+  ];
+}
--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -4,51 +4,53 @@ import { Layout, Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import { useTranslation } from "react-i18next";
-import { getItem } from "../../../commen/functions/utils.js";
+import { generateMenuItems, getItem } from "../../../commen/functions/utils.js";
 const { Sider } = Layout;
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  let roleID = localStorage.getItem("roleID");
+  const items = generateMenuItems(roleID, t);
   const [openKeys, setOpenKeys] = useState(["sub1"]);
   const [selectedKeys, setSelectedKeys] = useState(
     localStorage.getItem("selectedKeys") === null
       ? ["2"]
       : [localStorage.getItem("selectedKeys")]
   );
-  const items = [
-    getItem(
-      <div className="setup-text-style">{t("Setups")}</div>,
-      "sub1",
-      <i className="icon-settings side-bar-icons"></i>,
-      [
-        getItem(t("Shift"), "2"),
-        getItem(t("Counter-admin"), "3"),
-        getItem(t("Branch-service"), "4"),
-        getItem(t("City-admin"), "5"),
-        getItem(t("City-wise-branch-services"), "6"),
-        getItem(t("City-branch-wise-shifts"), "7"),
-        getItem(t("City-branch-shifts-wise-counter"), "8"),
-        getItem(t("Employee-main"), "9"),
-        getItem(t("Country-city-wise-counter"), "10"),
-        getItem(t("Country-main"), "11"),
-      ],
-      "menu-item-sub"
-    ),
-    getItem(
-      <div className="setup-text-style">{t("Reports")}</div>,
-      "sub2",
-      <i className="icon-file side-bar-icons"></i>,
-      [
-        getItem(t("Country-wise-reports"), "12"),
-        getItem(t("City-wise-reports"), "13"),
-        getItem(t("Branch-wise-reports"), "14"),
-        getItem(t("Employee-wise-reports"), "15"),
-        getItem(t("Service-wise-reports"), "16"),
-      ],
-      "menu-item-sub"
-    ),
-  ];
+  // const items = [
+  //   getItem(
+  //     <div className="setup-text-style">{t("Setups")}</div>,
+  //     "sub1",
+  //     <i className="icon-settings side-bar-icons"></i>,
+  //     [
+  //       getItem(t("Shift"), "2"),
+  //       getItem(t("Counter-admin"), "3"),
+  //       getItem(t("Branch-service"), "4"),
+  //       getItem(t("City-admin"), "5"),
+  //       getItem(t("City-wise-branch-services"), "6"),
+  //       getItem(t("City-branch-wise-shifts"), "7"),
+  //       getItem(t("City-branch-shifts-wise-counter"), "8"),
+  //       getItem(t("Employee-main"), "9"),
+  //       getItem(t("Country-city-wise-counter"), "10"),
+  //       getItem(t("Country-main"), "11"),
+  //     ],
+  //     "menu-item-sub"
+  //   ),
+  //   getItem(
+  //     <div className="setup-text-style">{t("Reports")}</div>,
+  //     "sub2",
+  //     <i className="icon-file side-bar-icons"></i>,
+  //     [
+  //       getItem(t("Country-wise-reports"), "12"),
+  //       getItem(t("City-wise-reports"), "13"),
+  //       getItem(t("Branch-wise-reports"), "14"),
+  //       getItem(t("Employee-wise-reports"), "15"),
+  //       getItem(t("Service-wise-reports"), "16"),
+  //     ],
+  //     "menu-item-sub"
+  //   ),
+  // ];
   const handleOpenChange = (keys) => {
     // Ensure only one submenu is open at a time
     if (keys.length > 0) {
