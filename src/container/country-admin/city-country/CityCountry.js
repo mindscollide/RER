f33acb1f18154d77bd8ee7af421b39fbba56b61a
aunnaqvi <aunnaqvi747@gmail.com>
2023-12-11 10:51:10 +0500

    code push
    code push 13

--- src/container/country-admin/city-country/CityCountry.js
+++ src/container/country-admin/city-country/CityCountry.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import "./Shift.css";
+import "./CityCountry.css";
 import {
   Paper,
   TextField,
@@ -8,11 +8,9 @@ import {
   Button,
   Table,
 } from "../../../components/elements";
-import DatePicker from "react-multi-date-picker";
-import TimePicker from "react-multi-date-picker/plugins/time_picker";
 import { useTranslation } from "react-i18next";
 
-const BranchAdmin = () => {
+const CityCountry = () => {
   const [isCheckboxSelected, setIsCheckboxSelected] = useState(false);
   const { t } = useTranslation();
 
@@ -52,39 +50,15 @@ const BranchAdmin = () => {
       dataIndex: "shiftName",
       key: "shiftName",
     },
-    {
-      title: <span className="table-text">{t("Start-time")}</span>,
-      dataIndex: "startTime",
-      key: "startTime",
-    },
-    {
-      title: <span className="table-text">{t("End-time")}</span>,
-      dataIndex: "endTime",
-      key: "endTime",
-    },
+
     {
       title: <span className="table-text">{t("Active")}</span>,
       dataIndex: "active",
       key: "active",
-      align: "center",
       render: (text, record) => (
         <>
           <span>
             <i className="icon-check icon-check-color"></i>
-          </span>
-        </>
-      ),
-    },
-    {
-      title: <span className="table-text">{t("Close")}</span>,
-      dataIndex: "Close",
-      key: "Close",
-      align: "center",
-
-      render: (text, record) => (
-        <>
-          <span>
-            <i className="icon-close icon-check-close-color"></i>
           </span>
         </>
       ),
@@ -98,6 +72,10 @@ const BranchAdmin = () => {
           <span className="icon-spaceing-dlt-edit">
             <i className="icon-text-edit icon-EDT-DLT-color"></i>
             <i className="icon-close icon-EDT-DLT-color"></i>
+            <i className="icon-settings icon-EDT-DLT-color"></i>
+            <i className="icon-branch icon-EDT-DLT-color"></i>
+            <i className="icon-counter icon-EDT-DLT-color"></i>
+            <i className="icon-repeat icon-EDT-DLT-color"></i>
           </span>
         </>
       ),
@@ -110,7 +88,7 @@ const BranchAdmin = () => {
         <Row>
           <Col lg={6} md={6} sm={6} className="d-flex justify-content-start">
             <span className="shift-heading">
-              {t("Shift")}
+              {t("City")}
               <span className="shift-sub-heading">
                 {" "}
                 {t("Saudi-arabia-riyadh")}
@@ -125,84 +103,61 @@ const BranchAdmin = () => {
         </Row>
         <Row className="mt-3">
           <Col lg={12} md={12} sm={12}>
-            <Paper className="Branch-Admin-paper">
+            <Paper className="Counter-Admin-paper">
               <Row>
                 <Col lg={6} md={6} sm={6}>
-                  <span className="text-labels">{t("Shift-name")}</span>
+                  <span className="text-labels">{t("City-name")}</span>
                   <TextField
                     name="Shift"
-                    placeholder={t("Shift-name")}
+                    placeholder={t("City-name")}
                     labelClass="d-none"
-                    className="text-fiels-Branch"
+                    className="text-fiels-CityCountry"
                   />
                 </Col>
 
                 <Col lg={6} md={6} sm={6}>
-                  <span className="text-labels">{t("Shift-name")}</span>
+                  <span className="text-labels">{t("City-name")}</span>
                   <TextField
                     name="Shift"
-                    placeholder={t("Shift-name")}
+                    placeholder={t("City-name")}
                     labelClass="d-none"
-                    className="text-fiels-Branch"
+                    className="text-fiels-CityCountry"
                   />
                 </Col>
               </Row>
 
               <Row className="mt-3">
-                <Col lg={2} md={2} sm={2} className="mt-4">
+                <Col lg={6} md={6} sm={6} className="mt-2">
                   <Checkbox
                     checked={isCheckboxSelected}
                     onChange={handleCheckboxChange}
-                    classNameDiv="Branch-checkbox"
+                    classNameDiv="CityCountry-checkbox"
                     label={
                       <span className="checkbox-label">{t("Active")}</span>
                     }
                   />
                 </Col>
-                <Col lg={5} md={5} sm={5} className="col-for-date-timepicker">
-                  <label className="text-labels">{t("Shift-start-time")}</label>
-                  <DatePicker
-                    arrowClassName="arrowClass"
-                    containerClassName="containerClassTimePicker"
-                    disableDayPicker
-                    format="hh:mm A"
-                    plugins={[<TimePicker hideSeconds />]}
-                  />
-                </Col>
 
-                <Col lg={5} md={5} sm={5} className="col-for-date-timepicker">
-                  <label className="text-labels">{t("Shift-end-time")}</label>
-                  <DatePicker
-                    arrowClassName="arrowClass"
-                    containerClassName="containerClassTimePicker"
-                    disableDayPicker
-                    format="hh:mm A"
-                    plugins={[<TimePicker hideSeconds />]}
-                  />
-                </Col>
-              </Row>
-
-              <Row className="my-3">
-                <Col lg={12} md={12} sm={12} className="btn-class-branch">
+                <Col lg={6} md={6} sm={6} className="btn-col-class">
                   <Button
                     icon={<i className="icon-add-circle icon-space"></i>}
                     text={t("Add")}
-                    className="Add-btn-Branch"
+                    className="Add-btn-CityCountry"
                   />
                   <Button
                     icon={<i className="icon-refresh icon-space"></i>}
                     text={t("Reset")}
-                    className="Reset-btn-Branch"
+                    className="Reset-btn-CityCountry"
                   />
                 </Col>
               </Row>
-
-              <Row>
+              <Row className="mt-2">
                 <Col lg={12} md={12} sm={12}>
                   <Table
                     rows={dataSource}
                     column={columns}
                     pagination={false}
+                    // className="table-text"
                   />
                 </Col>
               </Row>
@@ -214,4 +169,4 @@ const BranchAdmin = () => {
   );
 };
 
-export default BranchAdmin;
+export default CityCountry;
