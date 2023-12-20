f2dba0a47be0872bfc1e7e6731d93f9a1bc357ce
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-12-20 12:12:18 +0500

    date pickers has been setteled

--- src/container/branch-reports/appoinment-reports-branch/AppoinmentReportBranch.js
+++ src/container/branch-reports/appoinment-reports-branch/AppoinmentReportBranch.js
@@ -44,7 +44,12 @@ const AppoinmentReportBranch = () => {
     useState([]);
 
   const [fromDate, setFromDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
+  const [toDate, setToDate] = useState(() => {
+    const today = new Date();
+    const futureDate = new Date(today.getTime() + 10 * 24 * 60 * 60 * 1000);
+
+    return futureDate;
+  });
 
   const dataSource = [
     {
