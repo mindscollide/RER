39b854dce5b1588ff7914c28db0ecd06504e1a61
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2023-12-20 17:34:46 +0500

    FILE CREATED

---
+++ src/container/city-admin/appointment-report-city/AppointmentReportCity.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./AppointmentReportCity.css";
+const AppointmentReportCity = () => {
+  return <div>AppointmentReportCity</div>;
+};
+
+export default AppointmentReportCity;
